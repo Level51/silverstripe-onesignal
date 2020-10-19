fe03aeabb113dbdf2da54e4d1b49967f00ee886d
Julian Scheuchenzuber <js@lvl51.de>
2020-10-19 10:08:04 +0200

    Add test for OneSignalService create

--- tests/OneSignalServiceTest.php
+++ tests/OneSignalServiceTest.php
@@ -2,11 +2,34 @@
 
 namespace Level51\OneSignal;
 
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Environment;
 use SilverStripe\Dev\SapphireTest;
 
 class OneSignalServiceTest extends SapphireTest {
 
-    public function testCreateNotification400() {
+    public function testMissingEnv1() {
+        $this->expectException(OneSignalException::class);
+
+        Environment::setEnv('ONESIGNAL_APP_AUTH_KEY', null);
+        OneSignalService::create();
+    }
+
+    public function testMissingEnv2() {
+        $this->expectException(OneSignalException::class);
+
+        Environment::setEnv('ONESIGNAL_AUTH_KEY', null);
+        OneSignalService::create();
+    }
+
+    public function testMissingConfig() {
+        $this->expectException(OneSignalException::class);
+
+        Config::forClass(OneSignalService::class)->remove('app_id');
+        OneSignalService::create();
+    }
+
+    public function testCreateNotificationError() {
         $response = OneSignalService::singleton()->createNotification(Notification::create());
 
         $this->assertTrue($response->isError());
